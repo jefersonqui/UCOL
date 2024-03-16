7ce54d9972588291e0eaca077911a7b91d9451bd
Jeferson Quiguantar <jefferson.qui.dmi@gmail.com>
2024-03-16 13:20:13 -0500

    Configuracion banner responsive2

--- src/pages/banner.jsx
+++ src/pages/banner.jsx
@@ -2,30 +2,21 @@ import stylesb from './banner.module.css';
 import bannerSvg from '../assets/banner.svg';
 const Banner = () => {
   return (
-
-    <section className={stylesb.sectionban}>
-      <div className={stylesb["banner-column"]}>
-      <img
-            className={stylesb["banner-image"]}
-            src={bannerSvg}
-            alt="Banner"
-            onMouseEnter={(e) => e.target.style.transform = 'translateY(-5px)'}
-            onMouseLeave={(e) => e.target.style.transform = 'translateY(0)'}
-          />
-        <div className={stylesb.bannerInner}>
-          <h1 className={stylesb["heading-xl"]}>Mapa Interactivo De Colombia</h1>
-          <p className={stylesb.paragraph}>
-          ¡Explora el conocimiento que se extiende por cada rincón de nuestra nación!
-          </p>
-          <button className={`${stylesb.btn} ${stylesb.btnDarken} ${stylesb.btnInline}`}>
-            Ver Mapa
-          </button>
-
-
+    <section class={stylesb.sectionban}>
+      <div class={stylesb["banner-container"]}>
+        <div class={stylesb["banner-column"]}>
+          <div class={stylesb.bannerInner}>
+            <h1 class={stylesb["heading-xl"]}>Mapa Interactivo De Colombia</h1>
+            <p class={stylesb.paragraph}>¡Explora el conocimiento que se extiende por cada rincón de nuestra nación!</p>
+            <button class={`${stylesb.btn} ${stylesb.btnDarken} ${stylesb.btnInline}`}>Ver Mapa<i className="bx bx-right-arrow-alt"></i></button>
+          </div>
         </div>
-
+        <div class={stylesb["image-container"]}>
+          <img class={stylesb["banner-image"]} src={bannerSvg} alt="Banner" />
+        </div>
       </div>
     </section>
+
 
   );
 };
