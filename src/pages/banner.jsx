4a1772b75a7ed7a024e891f93d1c81f4b4eb8c01
Jeferson Quiguantar <jefferson.qui.dmi@gmail.com>
2024-03-16 11:52:23 -0500

    Configuracion banner responsive

--- src/pages/banner.jsx
+++ src/pages/banner.jsx
@@ -1,24 +1,32 @@
-import stylesBanner from './banner.module.css';
+import stylesb from './banner.module.css';
 import bannerSvg from '../assets/banner.svg';
 const Banner = () => {
   return (
-    <div className={stylesBanner.container}>
-      <div className={stylesBanner.div__container}>
-        <h1 className={stylesBanner.titleh1}>Mapa Interactivo De Colombia</h1>
-        <p className={stylesBanner.subtitle}>¡Explora el conocimiento que se extiende por cada rincón de nuestra nación!</p>
-      </div>
-      <aside className={stylesBanner.asidecont}>
-        <div className={stylesBanner.contimg}>
-        <img 
-        className={stylesBanner.bannerimg} 
-        src={bannerSvg} 
-        alt="Banner"
-        onMouseEnter={(e) => e.target.style.transform = 'translateY(-5px)'}
-        onMouseLeave={(e) => e.target.style.transform = 'translateY(0)'}
-        />
+
+    <section className={stylesb.sectionban}>
+      <div className={stylesb["banner-column"]}>
+      <img
+            className={stylesb["banner-image"]}
+            src={bannerSvg}
+            alt="Banner"
+            onMouseEnter={(e) => e.target.style.transform = 'translateY(-5px)'}
+            onMouseLeave={(e) => e.target.style.transform = 'translateY(0)'}
+          />
+        <div className={stylesb.bannerInner}>
+          <h1 className={stylesb["heading-xl"]}>Mapa Interactivo De Colombia</h1>
+          <p className={stylesb.paragraph}>
+          ¡Explora el conocimiento que se extiende por cada rincón de nuestra nación!
+          </p>
+          <button className={`${stylesb.btn} ${stylesb.btnDarken} ${stylesb.btnInline}`}>
+            Ver Mapa
+          </button>
+
+
         </div>
-      </aside>
-    </div>
-  )
-}
+
+      </div>
+    </section>
+
+  );
+};
 export default Banner;
